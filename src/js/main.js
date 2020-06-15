6e78dd1dc96d9d4ab8b66bde2b142f8bf1262c96
inovikov11 <igor.niko.novikov@mail.ru>
2020-06-15 21:54:56 +0300

    Сверстана и адаптирована секция Контакты, подключен функционал меню

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+  //Подключаем меню
   $('#menu-button').click(function() {
     $('.mobile').toggleClass('mobile-active');
     $('.mobile').css("display", "block");
@@ -9,4 +10,51 @@ $(function(){
     $('.mobile').css("display", "none");
     $("html,body").css("overflow","auto");
   });
+  //Функционал меню
+  $getOffset = function($obj) {
+    $objTop = $($obj).offset();
+    $top = $objTop.top;
+    return $top;
+  };
+
+  $('.menu-home').click(function() {
+    $objTopNum = $getOffset('.hero');
+    $('body,html').animate({scrollTop:$objTopNum},800);
+    $('.mobile').toggleClass('mobile-active');
+    $('.mobile').css("display", "none");
+    $("html,body").css("overflow","auto");
+  });
+
+  $('.menu-about').click(function() {
+    $objTopNum = $getOffset('.about');
+    $('body,html').animate({scrollTop:$objTopNum},800);
+    $('.mobile').toggleClass('mobile-active');
+    $('.mobile').css("display", "none");
+    $("html,body").css("overflow","auto");
+  });
+
+  $('.menu-skills').click(function() {
+    $objTopNum = $getOffset('.skills');
+    $('body,html').animate({scrollTop:$objTopNum},800);
+    $('.mobile').toggleClass('mobile-active');
+    $('.mobile').css("display", "none");
+    $("html,body").css("overflow","auto");
+  });
+
+  $('.menu-portfolio').click(function() {
+    $objTopNum = $getOffset('.portfolio');
+    $('body,html').animate({scrollTop:$objTopNum},800);
+    $('.mobile').toggleClass('mobile-active');
+    $('.mobile').css("display", "none");
+    $("html,body").css("overflow","auto");
+  });
+
+  $('.menu-contacts').click(function() {
+    $objTopNum = $getOffset('.footer');
+    $('body,html').animate({scrollTop:$objTopNum},800);
+    $('.mobile').toggleClass('mobile-active');
+    $('.mobile').css("display", "none");
+    $("html,body").css("overflow","auto");
+  });
+  
 });
